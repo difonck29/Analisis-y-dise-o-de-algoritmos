2fe00500cd9b2de7f918b415d74e6c0337fbde9d
Diego Ivan Fonseca <difonck29@gmail.com>
2012-08-10 11:48:16 -0500

    cod: 624736

---
+++ mergesort1.py
@@ -0,0 +1,66 @@
+import time
+import random
+
+def funcion(n):
+	if n==0:
+		print("Hola")
+	print n
+	if n<1:
+		exit()
+	funcion(n-1)
+
+
+def merge(left, right):
+	result = []
+	i ,j = 0, 0
+    	while i < len(left) and j < len(right):
+        	if left[i] <= right[j]:
+            		result.append(left[i])
+            		i += 1
+       		else:
+            		result.append(right[j])
+            		j += 1
+    	result += left[i:]
+    	result += right[j:]
+    	return result
+
+def mergesort(list):
+	if len(list) < 2:
+		return list
+    	middle = len(list) / 2
+    	left = mergesort(list[:middle])
+    	right = mergesort(list[middle:])
+    	return merge(left, right)
+
+vector1=[10,9,8,7,6,5,4,3,2,1]
+vector2=[20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+vector3=[]
+for i in range(0,100):
+       numero = int(random.randint(0,1000))
+       vector3.append(numero)
+
+
+#operacion mergesort con vector de 10 posiciones
+
+
+print"__________________________________________"
+print "Mergesort vector 10 posiciones"
+print
+inicio=time.time();
+print mergesort(vector1)
+fin=time.time()
+print
+print "El tiempo fue : ",fin-inicio
+print"__________________________________________"
+
+#Operacion mergesort con vector de 20 posiciones
+
+print"__________________________________________"
+print "Mergesort vector 20 posiciones"
+print
+inicio=time.time();
+print mergesort(vector2)
+fin=time.time()
+print
+print "El tiempo fue : ",fin-inicio
+print"__________________________________________"
